5dd45a21672228aae2df4d7e7787e1bf388625d7
Devam Doshi <devamdoshi212@gmail.com>
2022-12-23 21:40:23 +0530

    Create Bubble_Sort.cpp

---
+++ Bubble_Sort.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+void bubblesort(int a[],int n)
+{
+    int flag,temp;
+    for(int i=0;i<n-1;i++)
+    {
+        flag=0;
+        for(int j=0;j<n-1;j++)
+        {
+            if(a[j]>a[j+1])
+            {
+                temp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=temp;
+                flag=1;
+            }
+        }
+        if(flag==0)
+        break;
+    } 
+}
+int main()
+{
+    int a[]={1,2,3,5,4,6,9,7,8};
+    int n=9;
+    bubblesort(a,n);
+    for(int i=0;i<n;i++)
+    {
+        cout<<a[i]<<" ";
+    }
+    return 0;
+}
